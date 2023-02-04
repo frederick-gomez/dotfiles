037d62b2f0e5dcc538227ce0b18c8ebb78935486
Fede Gomez <gomez.fede98@gmail.com>
2023-02-04 14:19:37 -0300

    Initial .config files

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar bottom &
+
+# Launch bar1
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
